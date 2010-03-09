5ee71cf99bdde1a00572b3924ae53e7625d720a8
enr <enrico.comiti@gmail.com>
2010-03-09 20:17:48 +0100

    applied eclipse profile

--- src/main/java/org/markdownj/extras/MarkdownServiceImpl.java
+++ src/main/java/org/markdownj/extras/MarkdownServiceImpl.java
@@ -21,69 +21,69 @@ import com.petebevin.markdown.MarkdownProcessor;
 /**
  * Basic implementation of MarkdownService.
  * 
- *
+ * 
  */
 public class MarkdownServiceImpl implements MarkdownService {
-	
-	/**
-	 * The string containing the html used as header.
-	 */
-	private String header;
-	
-	/**
-	 * The string containing the html used as footer.
-	 */
-	private String footer;
-	
-	/**
-	 * The string containing the markdown to transform.
-	 */
-	private String content;
 
-	public MarkdownServiceImpl() {
-	}
+    /**
+     * The string containing the html used as header.
+     */
+    private String header;
 
-	public String process() {
-		MarkdownProcessor processor = new MarkdownProcessor();
-		String hs = (header == null) ? "" : header;
-		String fs = (footer == null) ? "" : footer;
-		return String.format("%s%s%s", hs, processor.markdown(content), fs);
-	}
+    /**
+     * The string containing the html used as footer.
+     */
+    private String footer;
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    /**
+     * The string containing the markdown to transform.
+     */
+    private String content;
 
-	public void setContentPath(String path) {
-		content = FileUtils.readFileFromPath(path);
-	}
+    public MarkdownServiceImpl() {
+    }
 
-	public void setContentUrl(URL url) {
-		content = FileUtils.readFileFromUrl(url);
-	}
+    public String process() {
+        MarkdownProcessor processor = new MarkdownProcessor();
+        String hs = (header == null) ? "" : header;
+        String fs = (footer == null) ? "" : footer;
+        return String.format("%s%s%s", hs, processor.markdown(content), fs);
+    }
 
-	public void setFooterPath(String path) {
-		footer = FileUtils.readFileFromPath(path);
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public void setFooterUrl(URL url) {
-		footer = FileUtils.readFileFromUrl(url);
-	}
+    public void setContentPath(String path) {
+        content = FileUtils.readFileFromPath(path);
+    }
 
-	public void setHeaderPath(String path) {
-		header = FileUtils.readFileFromPath(path);
-	}
+    public void setContentUrl(URL url) {
+        content = FileUtils.readFileFromUrl(url);
+    }
 
-	public void setHeaderUrl(URL url) {
-		header = FileUtils.readFileFromUrl(url);
-	}
+    public void setFooterPath(String path) {
+        footer = FileUtils.readFileFromPath(path);
+    }
 
-	public void setFooter(String footer) {
-		this.footer = footer;
-	}
+    public void setFooterUrl(URL url) {
+        footer = FileUtils.readFileFromUrl(url);
+    }
 
-	public void setHeader(String header) {
-		this.header = header;
-	}
+    public void setHeaderPath(String path) {
+        header = FileUtils.readFileFromPath(path);
+    }
+
+    public void setHeaderUrl(URL url) {
+        header = FileUtils.readFileFromUrl(url);
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
 
 }
--- src/test/java/org/markdownj/extras/AppTest.java
+++ src/test/java/org/markdownj/extras/AppTest.java
@@ -18,8 +18,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,72 +28,130 @@ import org.junit.Test;
 /**
  * Integration test for MarkdownApp
  */
-public class AppTest 
-{
+public class AppTest {
 
-	private String headerPath;
-	private String footerPath;
-	private String sourcePath;
-	
-	private String destination;
+    private String headerPath;
+    private String footerPath;
+    private String sourcePath;
 
-	@Before
-	public void initData() {
-		
-		headerPath = resourceToPath("/site/templates/header.html");
-		footerPath = resourceToPath("/site/templates/footer.html");
-		sourcePath = resourceToPath("/site/markdown");
-		
-		destination = "target/markdownj";
-	}
-	
-	@Test
-	public void testMainMethodCall() throws IOException {
-		clearDirectory(destination);
-		String[] args = {"--source", sourcePath, "--destination", destination, "--header", headerPath, "--footer", footerPath};
-		MarkdownApp.main(args);
-		File destinationFile = new File(destination+"/sub/file.html");
-		assertTrue(destinationFile.exists());
-		assertEquals("<html>\n<h1>This is an H1</h1>\n\n<p>file.markdown</p>\n\n</html>\n", FileUtils.readFileFromPath(destinationFile.getPath()));
-	}
+    private String baseTestDestination;
 
-	@Test
-	public void testProgrammaticCall() throws IOException {
-		clearDirectory(destination);
-		MarkdownApp app = new MarkdownApp();
-		app.setSource(sourcePath);
-		app.setDestination(destination);
-		app.setHeader(headerPath);
-		app.setFooter(footerPath);
-		app.process();
-		File destinationFile = new File(destination+"/sub/file.html");
-		assertTrue(destinationFile.exists());
-		assertEquals("<html>\n<h1>This is an H1</h1>\n\n<p>file.markdown</p>\n\n</html>\n", FileUtils.readFileFromPath(destinationFile.getPath()));
-	}
-	
-	/**
-	 * Utility method to resolve path to resources.
-	 * @param resource
-	 * @return
-	 */
-	private String resourceToPath(String resource)
-	{
-		URL url = this.getClass().getResource(resource);
-		File uf = new File(url.getFile());
-		return FileUtils.normalizedPath(uf.getAbsolutePath());
-	}
-	
-	/**
-	 * Utility method to delete all content of a directory, if it exists.
-	 * 
-	 * @param path
-	 * @throws IOException 
-	 */
-	private void clearDirectory(String path) throws IOException
-	{
-		File dir = new File(path);
-		if (dir.exists()) {
-			FileUtils.deleteDirectoryContents(dir);
-		}
-	}
+    @Before
+    public void initData() {
+
+        headerPath = resourceToPath("/site/templates/header.html");
+        footerPath = resourceToPath("/site/templates/footer.html");
+        sourcePath = resourceToPath("/site/markdown");
+
+        baseTestDestination = "target/markdownj";
+    }
+
+    @Test
+    public void testMainMethodCall() {
+        String destination = buildDestinationDir("testMainMethodCall");
+        String[] args = { "--source", sourcePath, "--destination", destination, "--header", headerPath, "--footer", footerPath };
+        MarkdownApp.main(args);
+        File destinationFile = new File(destination + "/sub/file.html");
+        assertTrue(destinationFile.exists());
+        assertEquals("<html>\n<h1>This is an H1</h1>\n\n<p>file.markdown</p>\n\n</html>\n", FileUtils.readFileFromPath(destinationFile.getPath()));
+    }
+
+
+    @Test
+    public void testMainMethodCallWithExtensionsArgument() {
+        String destination = buildDestinationDir("testMainMethodCallWithExtensionsArgument");
+        String extensions = "markdown,text";
+        String[] args = { "--source", sourcePath, "--destination", destination, "--header", headerPath, "--footer", footerPath, "--extensions", extensions};
+        MarkdownApp.main(args);
+        File destinationFile = new File(destination + "/sub/file.html");
+        assertTrue(destinationFile.exists());
+        assertEquals("<html>\n<h1>This is an H1</h1>\n\n<p>file.markdown</p>\n\n</html>\n", FileUtils.readFileFromPath(destinationFile.getPath()));
+        File mdExtDestinationFile = new File(destination + "/sub/md-ext.html");
+        assertTrue(String.format("File with extension 'md' processed, but processable extensions list is: '%s'", extensions),
+                !mdExtDestinationFile.exists());
+    }
+    
+    @Test
+    public void testProgrammaticCall() {
+        String destination = buildDestinationDir("testProgrammaticCall");
+        MarkdownApp app = new MarkdownApp();
+        app.setSource(sourcePath);
+        app.setDestination(destination);
+        app.setHeader(headerPath);
+        app.setFooter(footerPath);
+        app.process();
+        File destinationFile = new File(destination + "/sub/file.html");
+        assertTrue(destinationFile.exists());
+        assertEquals("<html>\n<h1>This is an H1</h1>\n\n<p>file.markdown</p>\n\n</html>\n", FileUtils.readFileFromPath(destinationFile.getPath()));
+        File mdExtDestinationFile = new File(destination + "/sub/md-ext.html");
+        assertTrue(mdExtDestinationFile.exists());
+    }
+
+    @Test
+    public void testNoProcessableExtensions() {
+        String destination = buildDestinationDir("testNoProcessableExtensions");
+        File mdExtDestinationFile = new File(destination + "/sub/md-ext.html");
+        MarkdownApp app = new MarkdownApp();
+        app.setSource(sourcePath);
+        app.setDestination(destination);
+        app.setHeader(headerPath);
+        app.setFooter(footerPath);
+        app.process();
+        assertTrue("File with extension 'md' not processed", mdExtDestinationFile.exists());
+    }
+
+    @Test
+    public void testFileWithNoProcessableExtension() {
+        String destination = buildDestinationDir("testFileWithNoProcessableExtension");
+        File mdExtDestinationFile = new File(destination + "/sub/md-ext.html");
+        MarkdownApp app = new MarkdownApp();
+        app.setSource(sourcePath);
+        app.setDestination(destination);
+        app.setHeader(headerPath);
+        app.setFooter(footerPath);
+        List<String> exts = new ArrayList<String>();
+        exts.add("markdown");
+        app.setProcessableExtensions(exts);
+        app.process();
+        assertTrue(String.format("File with extension 'md' processed, but processable extensions are: %s", app.getProcessableExtensions()),
+                !mdExtDestinationFile.exists());
+    }
+
+    @Test
+    public void testFileWithProcessableExtension() {
+        String destination = buildDestinationDir("testFileWithProcessableExtension");
+        File mdExtDestinationFile = new File(destination + "/sub/md-ext.html");
+        MarkdownApp app = new MarkdownApp();
+        app.setSource(sourcePath);
+        app.setDestination(destination);
+        app.setHeader(headerPath);
+        app.setFooter(footerPath);
+        app.addProcessableExtension("md");
+        app.process();
+        assertTrue("File with extension 'md' not processed", mdExtDestinationFile.exists());
+    }
+
+    /**
+     * Utility method to resolve path to resources.
+     * 
+     * @param resource
+     * @return
+     */
+    private String resourceToPath(String resource) {
+        URL url = this.getClass().getResource(resource);
+        File uf = new File(url.getFile());
+        return FileUtils.normalizedPath(uf.getAbsolutePath());
+    }
+
+    /**
+     * Utility method to create destination directories.
+     * 
+     * @param id
+     * @return
+     */
+    private String buildDestinationDir(String id) {
+        String destination = baseTestDestination + "/" + id;
+        return destination;
+    }
+
 }
